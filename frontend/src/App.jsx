1127c7fe926269f2ad77bb3087e074ef7bcad969
DamithuS <ddsamarasinha@students.nsbm.ac.lk>
2026-04-26 23:19:36 +0530

    Scaffold React + Vite frontend with routing and global styles

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import DashboardPage from './pages/DashboardPage'
+import ProfilePage from './pages/ProfilePage'
+import ProtectedRoute from './components/ProtectedRoute'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<LoginPage />} />
+
+      <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+      <Route path="/profile"   element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+    </Routes>
+  )
+}
